694590c0ee24bdc9f2fe387cac8cabc92c12f3ad
Mahnoor Ahmed <mahnoorkfa@gmail.com>
2026-09-25 06:47:02 +0500

    Implement reverseInPlace with two-pointer swap

---
+++ arrays/ArrayUtils.java
@@ -0,0 +1,35 @@
+public class ArrayUtils {
+
+  public static void reverseInPlace(int[] arr) {
+    int j = arr.length - 1;
+    int i = 0;
+
+    while (i < j) {
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      i++;
+      j--;
+    }
+
+  }
+
+  public static int findMax(int[] arr) {
+    // TODO
+  }
+
+  public static int findMin(int[] arr) {
+    // TODO
+  }
+
+  public static boolean hasDuplicates(int[] arr) {
+    // TODO
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5 };
+    reverseInPlace(arr);
+
+    System.out.println(java.util.Arrays.toString(arr));
+  }
+}
