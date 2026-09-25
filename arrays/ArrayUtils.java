d58ed1f6bbf60802965d4cdfc2a7ccbabe654232
Mahnoor Ahmed <mahnoorkfa@gmail.com>
2026-09-25 07:20:46 +0500

    Implement hasDuplicates with early return

--- arrays/ArrayUtils.java
+++ arrays/ArrayUtils.java
@@ -45,16 +45,29 @@ public class ArrayUtils {
   
 
   public static boolean hasDuplicates(int[] arr) {
-    
-    // TODO
+    if (arr.length == 0) {
+      throw new IllegalArgumentException("Cannot find min of an empty array");
+    }
+    for (int i=0; i<arr.length; i++){
+      for (int j=i+1; j< arr.length; j++){
+        if (arr[i]==arr[j]){
+         return true;
+         
+        }
+      
+    }
+
   }
+  return false;
+}
 
   public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4, 5 };
+    int[] arr = { 1, 2, 3, 5, 5 };
     reverseInPlace(arr);
     System.out.println(java.util.Arrays.toString(arr));
 
     System.out.println(findMax(arr));
     System.out.println(findMin(arr));
+    System.out.println(hasDuplicates(arr));
   }
 }
